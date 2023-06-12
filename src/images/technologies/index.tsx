19f4cb53fd6920fbf34f1894b9c719f02d979026
Andreas Zander <andreaszander@Andreass-MacBook-Pro.local>
2023-06-12 10:57:56 +0200

    Copy files from old repo

---
+++ src/images/technologies/index.tsx
@@ -0,0 +1,41 @@
+// Frontend Logos
+import CSSLogo from "./css.svg";
+import GatsbyLogo from "./gatsby.svg";
+import GraphqlLogo from "./graphql.svg";
+import HTMLLogo from "./html.svg";
+import JestLogo from "./jest.svg";
+import NextJSLogo from "./nextjs.svg";
+import NodeLogo from "./nodejs.svg";
+import ReactLogo from "./react-logo.svg";
+import ReduxLogo from "./redux.svg";
+import TypeScriptLogo from "./typescript.svg";
+
+// Backend Logos
+import AWSLambdaLogo from "./aws-lambda.svg";
+import JavaLogo from "./java.svg";
+import KotlinLogo from "./kotlin.svg";
+import KubernetesLogo from "./kubernetes.svg";
+import PythonLogo from "./python.svg";
+import SpringLogo from "./spring.svg";
+
+export const FrontendTechonlogyLogos = [
+	<CSSLogo />,
+	<GatsbyLogo />,
+	<GraphqlLogo />,
+	<HTMLLogo />,
+	<JestLogo />,
+	<NextJSLogo />,
+	<NodeLogo />,
+	<ReactLogo />,
+	<ReduxLogo />,
+	<TypeScriptLogo />,
+];
+
+export const BackendTechnologyLogos = [
+	<AWSLambdaLogo />,
+	<JavaLogo />,
+	<KotlinLogo />,
+	<KubernetesLogo />,
+	<PythonLogo />,
+	<SpringLogo />,
+];
