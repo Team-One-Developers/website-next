6eebd7a15b96ea5dcb883c8b440fb07cd375f364
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-07-20 12:38:46 +0200

    feat: add seo

---
+++ src/config/siteConfig.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "TeamOneDevelopers",
+    url: "https://teamonedevelopers.de",
+    ogImage: "https://teamonedevelopers.de/og.jpg",
+    description:
+      "Wir sind Technologie-Experten und verstehen uns als integrativer Entwicklungs- & Beratungspartner in komplexen Softwareprojekten. .",
+    links: {
+    //   twitter: "https://twitter.com/teamone",
+      github: "https://github.com/Team-One-Developers",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
