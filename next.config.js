c6d67483f2e74a890004f3237f4269dee5d517a1
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-07-27 16:00:00 +0200

    fix images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withContentlayer } = require("next-contentlayer")
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
@@ -9,4 +7,4 @@ const nextConfig = {
 	},
 }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig
