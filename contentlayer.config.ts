ddd292a1342c84a773e2f3ee853324d0ca94a9f4
Julian Richter <julian.richter@t1dev.de>
2024-06-06 10:42:49 +0200

    change Engineering to Software Engineering

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -143,7 +143,7 @@ export const Career = defineDocumentType(() => ({
         },
         division: {
             type: "enum",
-            options: ["Engineering", "Agile Transformation", "Operations", "Marketing"],
+            options: ["Software Engineering", "Agile Transformation", "Operations", "Marketing"],
             required: true
         },
         tags: { type: "list", of: { type: "string" }, required: true },
