0d46249516ddbea9d1216ec1e4ddaa457dcd4022
Julian Richter <julian.richter@t1dev.de>
2025-02-07 15:00:20 +0100

    get ip adress of sender

--- src/app/actions/sendSlackMessage.ts
+++ src/app/actions/sendSlackMessage.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { bolt } from "@/lib/slack"
+import { headers } from "next/headers"
 
 interface SlackResponse {
     success: boolean
@@ -19,6 +20,8 @@ export async function sendSlackMessage({ name, email, message }: FormData, chann
             return { success: false, error: "Message and channel are required" }
         }
 
+        const ipAddress = await getIpAddress()
+
         const result = await bolt.client.chat.postMessage({
             channel,
             blocks: [
@@ -36,13 +39,25 @@ export async function sendSlackMessage({ name, email, message }: FormData, chann
                     type: "section",
                     text: {
                         type: "mrkdwn",
-                        text: `*Name:*\n${name}\n*E-Mail:*\n${email}\n*Message:* ${message}\n`
+                        text: `*Name:*\n${name}\n*E-Mail:*\n${email}\n*Message:*\n${message}\n`
                     },
                     accessory: {
                         type: "image",
                         image_url: "https://api.slack.com/img/blocks/bkb_template_images/approvalsNewDevice.png",
                         alt_text: "computer thumbnail"
                     }
+                },
+                {
+                    type: "divider"
+                },
+                {
+                    type: "context",
+                    elements: [
+                        {
+                            type: "mrkdwn",
+                            text: `*IP Address:* ${ipAddress}`
+                        }
+                    ]
                 }
             ],
             text: `New Contact Form Submission: ${message}`,
@@ -65,4 +80,16 @@ export async function sendSlackMessage({ name, email, message }: FormData, chann
             error: (error as Error).message
         }
     }
+}
+
+const getIpAddress = async (): Promise<string | null> => {
+    const fallbackIpAddress = "0.0.0.0"
+    const headerList = await headers()
+    const forwardedFor = headerList.get("x-forwarded-for")
+
+    if (forwardedFor) {
+        return forwardedFor.split(",")[0] || fallbackIpAddress
+    }
+
+    return headerList.get("x-real-ip") ?? fallbackIpAddress
 }
