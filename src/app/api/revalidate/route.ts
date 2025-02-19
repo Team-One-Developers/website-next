04a4023fd4586ffeb4fb1d53d36893a5f31733a6
Andreas Zander <38784465+AndiZandi@users.noreply.github.com>
2025-02-19 14:45:34 +0100

    fixed: isr (#44)
    * fixed: isr
    
    * fixed: isr

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -20,12 +20,17 @@ export async function POST(req: NextRequest) {
         if (!body?.slug || !body._type || (body._type !== "blog" && body._type !== "career")) {
             return new Response("Bad Request", { status: 400 })
         }
+        const isDraftSlug = body.slug.startsWith("draft-")
 
         const overviewPagePath = `/${body._type === "blog" ? "blog" : "career"}`
-        const specificPath = `/${body._type === "blog" ? "blog" : "career/job"}/${body.slug}`
+
+        // we need to revalidate the possible other variant of the slug to remove old versions
+        const specificPath = `/${body._type === "blog" ? "blog" : "career/job"}/${isDraftSlug ? body.slug.slice(6) : body.slug}`
+        const specificPathDraft = `/${body._type === "blog" ? "blog" : "career/job"}/${isDraftSlug ? body.slug : "draft-" + body.slug}`
 
         await revalidatePath(specificPath)
         await revalidatePath(overviewPagePath)
+        await revalidatePath(specificPathDraft)
 
         // Vercel logging
         console.log(`/revalidate triggered succesfully. Revalidated for slug ${body.slug}`)
