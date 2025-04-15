f7656e2cdb3ef649174361999f850439ff9992c3
Andreas Zander <andreasderzander@gmail.com>
2025-04-15 16:50:45 +0200

    edited: use and adjust old career listing component

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,6 +1,8 @@
+import Typography from "@/components/atoms/Typography"
 import { PageLayout } from "@/components/layout/PageLayout"
+import { Section } from "@/components/layout/Section"
+import { CareerElement } from "@/components/molecules/CareerElement"
 import About9 from "@/components/tailgrid/About9"
-import Portfolio from "@/components/tailgrid/Portfolio"
 import Portfolio5 from "@/components/tailgrid/Portfolio5"
 import Testimonial3 from "@/components/tailgrid/Testimonial3"
 import { PAGE_THEME } from "@/constants"
@@ -8,6 +10,7 @@ import { client } from "@/sanity/lib/client"
 import { QUERY_ALL_CAREERS } from "@/sanity/queries"
 import { QUERY_ALL_CAREERSResult } from "@/sanity/types"
 import { Metadata } from "next"
+import React from "react"
 
 export const metadata: Metadata = {
     title: "Karriere",
@@ -40,7 +43,39 @@ const Career = async () => {
         <PageLayout theme={PAGE_THEME.light}>
             <About9 />
             <Portfolio5 />
-            <Portfolio careers={publicCareers} usedDivisions={Object.keys(sortedCareers)} />
+            <Section id="jobs">
+                <div>
+                    <Typography as="h2" variant="h1" className="lg:max-w-[60%]">
+                        Aktuelle Jobs:
+                    </Typography>
+                    <div>
+                        {Object.keys(sortedCareers).map((division, index) => {
+                            const category = sortedCareers[division]
+                            return (
+                                <React.Fragment key={division}>
+                                    <Typography
+                                        as="h3"
+                                        variant="h2"
+                                        className="mb-4 mt-12 font-normal text-primary lg:max-w-[60%]"
+                                        key={division}
+                                    >
+                                        {division}
+                                    </Typography>
+                                    <div>
+                                        {category?.map((career, index) => (
+                                            <CareerElement
+                                                listIndex={index + 1}
+                                                key={career.slug?.current}
+                                                career={career}
+                                            />
+                                        ))}
+                                    </div>
+                                </React.Fragment>
+                            )
+                        })}
+                    </div>
+                </div>
+            </Section>
             <Testimonial3 person="melanie" />
         </PageLayout>
     )
--- src/components/molecules/CareerElement.tsx
+++ src/components/molecules/CareerElement.tsx
@@ -13,9 +13,9 @@ export const CareerElement = ({ career, listIndex }: JobProps) => {
     return (
         <div className="border-t-2 border-white last:border-b-2">
             <Link href={`/career/job/${career.slug.current}`} aria-label={`Link zur Jobseite ${career.title}`}>
-                <div className="grid cursor-pointer grid-cols-1 gap-[4px] p-3 hover:bg-t1-darkGray md:grid-cols-[0.1fr_2fr_0.5fr] md:gap-0 md:p-0">
+                <div className="grid cursor-pointer grid-cols-1 gap-[4px] p-3 hover:bg-primary/50 md:grid-cols-[0.1fr_2fr_0.5fr] md:gap-0 md:p-0">
                     <div className="shrink-0 grow-0 basis-[2.5%] md:m-4">
-                        <Typography className="text-white" variant="description">
+                        <Typography className="" variant="description">
                             {listIndex.toString().padStart(2, "0")}
                         </Typography>
                     </div>
@@ -26,10 +26,10 @@ export const CareerElement = ({ career, listIndex }: JobProps) => {
                     </div>
                     <div className="flex flex-wrap md:m-4">
                         <div className="flex basis-full">
-                            <Typography className="uppercase text-white" variant="paragraph">
+                            <Typography className="uppercase" variant="paragraph">
                                 {career.employmentType},&nbsp;
                             </Typography>
-                            <Typography className="uppercase text-white" variant="paragraph">
+                            <Typography className="uppercase" variant="paragraph">
                                 {career.schedule}
                             </Typography>
                         </div>
