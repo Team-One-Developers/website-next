d092ababcbf8e82b4b82533d2b9294d6f4dbb56c
Julian Richter <julian.richter@t1dev.de>
2025-06-22 14:51:04 +0200

    move BlogAndNews to own component

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,23 +1,13 @@
+import { BlogAndNews } from "@/components/sections/BlogAndNews"
 import Customers from "@/components/sections/Customers"
-import Hero from "@/components/sections/Hero"
 import Hero from "@/components/sections/Hero/Hero"
 import ImageDivider from "@/components/sections/ImageDivider"
 import TextColumns from "@/components/sections/TextColumns"
 import TripleImageColumn from "@/components/sections/TripleImageColumn"
-import { client } from "@/sanity/lib/client"
-import { QUERY_NEWEST_BLOGS } from "@/sanity/queries"
-import { QUERY_NEWEST_BLOGSResult } from "@/sanity/types"
-import { formatDate } from "@/utils/formateDate"
 
-const Home = async () => {
-    const newestBlogs: QUERY_NEWEST_BLOGSResult = await client.fetch(
-        QUERY_NEWEST_BLOGS,
-        {},
-        { cache: process.env.NODE_ENV === "development" ? "no-store" : "force-cache" }
-    )
-
+const Home = () => {
     return (
-        <div>
+        <>
             <Hero
                 subtitle="Tech driven business transformation"
                 title="Strategisch, datengetrieben, umsetzungsstark."
@@ -74,34 +64,8 @@ const Home = async () => {
                 layout="default"
             />
             <ImageDivider fullWidth />
-            <TripleImageColumn
-                title="Blog & News Update"
-                columns={[
-                    {
-                        title: newestBlogs[0].title,
-                        text: newestBlogs[0].descriptionLong,
-                        image: newestBlogs[0].heroImage?.asset?.url || "/images/culture/coffetalk.webp",
-                        tags: [formatDate(newestBlogs[0].date)],
-                        href: `/blog/${newestBlogs[0].slug?.current}`
-                    },
-                    {
-                        title: newestBlogs[1].title,
-                        text: newestBlogs[1].descriptionLong,
-                        image: newestBlogs[1].heroImage?.asset?.url || "/images/culture/coffetalk.webp",
-                        tags: [formatDate(newestBlogs[1].date)],
-                        href: `/blog/${newestBlogs[1].slug?.current}`
-                    },
-                    {
-                        title: newestBlogs[2].title,
-                        text: newestBlogs[2].descriptionLong,
-                        image: newestBlogs[2].heroImage?.asset?.url || "/images/culture/coffetalk.webp",
-                        tags: [formatDate(newestBlogs[2].date)],
-                        href: `/blog/${newestBlogs[2].slug?.current}`
-                    }
-                ]}
-                layout="blog"
-            />
-        </div>
+            <BlogAndNews />
+        </>
     )
 }
 export default Home
---
+++ src/components/sections/BlogAndNews.tsx
@@ -0,0 +1,43 @@
+import TripleImageColumn from "@/components/sections/TripleImageColumn"
+import { client } from "@/sanity/lib/client"
+import { QUERY_NEWEST_BLOGS } from "@/sanity/queries"
+import { QUERY_NEWEST_BLOGSResult } from "@/sanity/types"
+import { formatDate } from "@/utils/formatDate"
+import { Suspense } from "react"
+
+export const BlogAndNews = async () => {
+    const cacheMode = process.env.NODE_ENV === "development" ? "no-store" : "force-cache"
+    const newestBlogs: QUERY_NEWEST_BLOGSResult = await client.fetch(QUERY_NEWEST_BLOGS, {}, { cache: cacheMode })
+
+    return (
+        <Suspense fallback={<div className="h-96 animate-pulse rounded-lg bg-gray-200" />}>
+            <TripleImageColumn
+                title="Blog & News Update"
+                columns={[
+                    {
+                        title: newestBlogs[0].title,
+                        text: newestBlogs[0].descriptionLong,
+                        image: newestBlogs[0].heroImage?.asset?.url || "/images/culture/coffetalk.webp",
+                        tags: [formatDate(newestBlogs[0].date)],
+                        href: `/blog/${newestBlogs[0].slug?.current}`
+                    },
+                    {
+                        title: newestBlogs[1].title,
+                        text: newestBlogs[1].descriptionLong,
+                        image: newestBlogs[1].heroImage?.asset?.url || "/images/culture/coffetalk.webp",
+                        tags: [formatDate(newestBlogs[1].date)],
+                        href: `/blog/${newestBlogs[1].slug?.current}`
+                    },
+                    {
+                        title: newestBlogs[2].title,
+                        text: newestBlogs[2].descriptionLong,
+                        image: newestBlogs[2].heroImage?.asset?.url || "/images/culture/coffetalk.webp",
+                        tags: [formatDate(newestBlogs[2].date)],
+                        href: `/blog/${newestBlogs[2].slug?.current}`
+                    }
+                ]}
+                layout="blog"
+            />
+        </Suspense>
+    )
+}
