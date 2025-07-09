a081f24960eba2ed4955bd378c52c20c581e6597
Andreas Zander <andreasderzander@gmail.com>
2025-07-09 17:44:18 +0200

    fixed: custom slugs

--- src/sanity/actions/slugPublish.ts
+++ src/sanity/actions/slugPublish.ts
@@ -14,21 +14,17 @@ export function createAsyncPublishAction(originalAction: DocumentActionComponent
             ...originalResult,
             onHandle: async () => {
                 // @ts-expect-error no time to fix this right now
-                const prevSlug = props.published!.slug?.current
-                // @ts-expect-error no time to fix this right now
-                const newSlug = props.draft!.slug?.current
+                const cleanSlug = props.draft!.slug?.current.replace(/^draft-/, "")
                 const title = props.draft!.title as string
                 const draftPrefix = props.draft!.visibility === "Draft" ? "draft-" : ""
 
-                // prevSlug !== newSlug ==> user adjusted the slug manually, so we trust them
-                // newSlug.replace(/^draft-/, "") !== slugify(title) ==> we remove any potential draft- prefix and then check if it is a custom slug, if so we want to keep it
-                if (prevSlug !== newSlug || newSlug.replace(/^draft-/, "") !== slugify(title)) {
-                    // make sure the draft prefix is correctly applied
-                    const slug = draftPrefix + newSlug.replace(/^draft-/, "")
-                    const path = `${props.type === "blog" ? "/blog" : props.type === "career" ? "/career/job" : ""}/${slug}`
+                // custom slug? ==> if so we want to keep it
+                if (cleanSlug !== slugify(title)) {
+                    const combinedSlug = draftPrefix + cleanSlug
+                    const path = `${props.type === "blog" ? "/blog" : props.type === "career" ? "/career/job" : ""}/${combinedSlug}`
                     const link = `https://www.team-one.de${path}`
 
-                    await patch.execute([{ set: { slug: { current: slug }, path, link } }])
+                    await patch.execute([{ set: { slug: { current: combinedSlug }, path, link } }])
                     originalResult.onHandle!()
                 }
                 // no custom slug, so we calculate it from the title and prefix it with draft- if needed
