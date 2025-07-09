75d58f9b0c80cfa071cc5b79a309682ee5a749ea
Andreas Zander <andreasderzander@gmail.com>
2025-07-09 15:30:51 +0200

    fixed: slugpublish edgecases. keep custom slugs

--- src/sanity/actions/slugPublish.ts
+++ src/sanity/actions/slugPublish.ts
@@ -14,19 +14,25 @@ export function createAsyncPublishAction(originalAction: DocumentActionComponent
             ...originalResult,
             onHandle: async () => {
                 // @ts-expect-error no time to fix this right now
-                // user adjusted the slug manually, so we trust them
-                if (props.published!.slug?.current !== props.draft!.slug?.current) {
-                    // @ts-expect-error no time to fix this right now
-                    const slug = props.draft!.slug?.current as string
+                const prevSlug = props.published!.slug?.current
+                // @ts-expect-error no time to fix this right now
+                const newSlug = props.draft!.slug?.current
+                const title = props.draft!.title as string
+                const draftPrefix = props.draft!.visibility === "Draft" ? "draft-" : ""
+
+                // prevSlug !== newSlug ==> user adjusted the slug manually, so we trust them
+                // newSlug.replace(/^draft-/, "") !== slugify(title) ==> we remove any potential draft- prefix and then check if it is a custom slug, if so we want to keep it
+                if (prevSlug !== newSlug || newSlug.replace(/^draft-/, "") !== slugify(title)) {
+                    // make sure the draft prefix is correctly applied
+                    const slug = draftPrefix + newSlug.replace(/^draft-/, "")
                     const path = `${props.type === "blog" ? "/blog" : props.type === "career" ? "/career/job" : ""}/${slug}`
                     const link = `https://www.team-one.de${path}`
 
                     await patch.execute([{ set: { slug: { current: slug }, path, link } }])
                     originalResult.onHandle!()
-                } else {
-                    const title = props.draft!.title as string
-                    const draftPrefix = props.draft!.visibility === "Draft" ? "draft-" : ""
-
+                }
+                // no custom slug, so we calculate it from the title and prefix it with draft- if needed
+                else {
                     const slug = draftPrefix + slugify(title)
                     const path = `${props.type === "blog" ? "/blog" : props.type === "career" ? "/career/job" : ""}/${slug}`
                     const link = `https://www.team-one.de${path}`
